e24fbb0d9705ba89c29d01818d528d610b2c565f
Anastasia Zagvozdkina <nastya0204z@gmail.com>
2026-04-17 17:35:07 +0500

    updated serializers

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -4,6 +4,11 @@ from apps.orders.models import Order, OrderItem
 from apps.reviews.models import Review
 from django.contrib.auth.models import User
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -14,12 +19,21 @@ class ProductOverviewSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'price', 'category']
 
-class OrderSerializer(serializers.ModelSerializer):
+class LoginSerializer(serializers.Serializer):
+    login = serializers.CharField(max_length=50)
+    password = serializers.CharField(max_length=50)
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=50)
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=50)
+
+class OrderSerializer(serializers.Serializer):
     class Meta:
         model = Order
         fields = '__all__'
 
-class OrderItemSerializer(serializers.ModelSerializer):
+class OrderItemSerializer(serializers.Serializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
